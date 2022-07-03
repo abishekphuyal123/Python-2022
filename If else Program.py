4899c83625865f3c984cf1b03e278f426d632a91
Abishek Phuyal <66534961+abishekphuyal123@users.noreply.github.com>
2022-07-03 10:59:48 +0100

    Add files via upload
    This is a simple if else program created in python. 
    #100 days of python. 
    #Day - 1

---
+++ If else Program.py
@@ -0,0 +1,17 @@
+x = int(input("Enter any number ( x ): "))
+
+y = int(input("Enter another number ( y ): "))
+
+if x > y:
+    print("X is greater: ")
+    
+elif x == y:
+    print("They are equal")
+    
+    
+    
+else:
+    print("Y is greater: ")
+    
+    
+    
